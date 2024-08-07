3115bb335c29fd663822b88d961e72f9f5ab991f
Sadiq2k <sadiqhussain2k23@gmail.com>
2024-08-07 22:32:25 +0530

    delete target file

--- src/test/java/QRcodeGenerater/QRcodeGeneraterApplicationTests.java
+++ src/test/java/QRcodeGenerater/QRcodeGeneraterApplicationTests.java
@@ -1,11 +1,9 @@
 package QRcodeGenerater;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@SpringBootConfiguration
 class QRcodeGeneraterApplicationTests {
 
 	@Test
