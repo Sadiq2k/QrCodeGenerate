41f10bf6fb70b2b2f2a34fb1acabfecf5d438acd
Sadiq2k <sadiqhussain2k23@gmail.com>
2024-08-07 22:27:34 +0530

    modified test file

--- src/test/java/QRcodeGenerater/QRcodeGeneraterApplicationTests.java
+++ src/test/java/QRcodeGenerater/QRcodeGeneraterApplicationTests.java
@@ -1,9 +1,11 @@
 package QRcodeGenerater;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@SpringBootConfiguration
 class QRcodeGeneraterApplicationTests {
 
 	@Test
